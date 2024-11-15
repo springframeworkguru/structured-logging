65d9468c79cca044b754ca7f495d54b7f97804aa
John Thompson <john@springframework.guru>
2024-11-15 07:37:31 -0500

    init add

---
+++ structured-logging/src/test/java/guru/springframework/structuredlogging/StructuredLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.structuredlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
